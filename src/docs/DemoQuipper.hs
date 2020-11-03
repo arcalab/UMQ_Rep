61dece9cf66b6c65051c070530a95d922afa6a1a
AnaNeri <ana.neri@nery.name>
2020-11-03 18:17:48 +0000

    end DemoQuipper

--- src/docs/DemoQuipper.hs
+++ src/docs/DemoQuipper.hs
@@ -5,160 +5,98 @@
 -- imports:
 
 import Quipper
+import Quipper.Internal
 
 import Quantum.Synthesis.Matrix
 import Quantum.Synthesis.Ring
-import QuipperLib.Synthesis
+import Quipper.Libraries.Synthesis
+
+import Data.Complex
+import Data.Ratio
+
+import Quipper.Libraries.Simulation
+
+
 
 ----------------------------------------------------------
 -- How to write your quantum circuit? --------------------
 ----------------------------------------------------------
 -- There are many ways to implement a quantum circuit
 -- * Manual implementation
--- * With build_circuit
 -- * With circuit's matrix
+-- * Simulation
 
 ----------------------------------------------------------
 -- * Manual implementation
 
-increment :: (Qubit, Qubit) -> Circ (Qubit, Qubit)
-increment (a1, a0) = do
-  qnot a0
-  qnot a1 `controlled` (a0 .==. 0)
-  return (a1, a0)
+mybell1 :: Bool -> Circ (Qubit, Qubit)
+mybell1 b = do 
+  q <- qinit b
+  x <- hadamard q
+  y <- qinit False
+  y <- qnot y `controlled` x
+  return (x,y)
 
-myoracle :: Qubit -> Qubit -> Qubit -> Circ Qubit
-myoracle a b c = do
-  comment "BEGIN myoracle"
-  a1 <- qinit 0
-  a0 <- qinit 0
-  label (a0, a1) "a"
-  label (a, b, c) ("a", "b", "c")
-  increment (a1, a0) `controlled` (a .==. 0)
-  increment (a1, a0) `controlled` (b .==. 0)
-  increment (a1, a0) `controlled` (a .==. 1)
-  increment (a1, a0) `controlled` (c .==. 0)
-  qnot a0 `controlled` (a1 .==. 0)
-  comment "END myoracle"
-  return a0
-
-r_circuit :: Qubit -> Qubit -> Qubit -> Circ ()
-r_circuit a b c = do
-  with_computed (myoracle a b c) $ \conflicts -> do
-    gate_Z conflicts
-    return ()
 
 main1 :: IO ()
 main1 = do
-  print_simple Preview r_circuit
-  
-------------------------------------------------------------
--- * With "build_circuit"
-
-type Int2 = (Bool, Bool)
-
-build_circuit
-increment2 :: Int2 -> Int2
-increment2 (a1, a0) =
- if a0 then (not a1, False) else (a1, True)
-
-build_circuit
-zero :: Int2
-zero = (False, False)
-
-build_circuit
-increment_if :: Bool -> Int2 -> Int2
-increment_if x y = if x then increment2 y else y
-
-build_circuit
-myfold :: (a -> b -> b) -> b -> [a] -> b
-myfold cons nil [] = nil
-myfold cons nil (x:xs) = cons x (myfold cons nil xs)
-
-build_circuit
-myoracle2 :: Bool -> Bool -> Bool -> Bool
-myoracle2 a b c = a0 == a1
- where
-   xs = [a == False, b == False, a == True, c == False]
-   (a1, a0) = myfold increment_if zero xs
-
-myoracle_unpack :: Qubit -> Qubit -> Qubit -> Circ Qubit
-myoracle_unpack a b c = do
- label (a,b,c) ("a", "b", "c")
- r <- unpack template_myoracle2 a b c
- label r "result"
- return r
-
-r_circuit2 :: Qubit -> Qubit -> Qubit -> Circ ()
-r_circuit2 a b c = do
-   with_computed (myoracle_unpack a b c) $ \conflicts -> do
-   gate_Z conflicts
-   return ()
-
-main2 :: IO ()
-main2 = do
- print_simple Preview r_circuit2
+  print_simple Preview (mybell1 False)
 
 --------------------------------------------------------------
 -- * From a matrix
 
-mymatrix :: Matrix Eight Eight Integer
-mymatrix = matrix_of_function f
-  where
-    f i j
-      | i /= j = 0
-      | cc `mod` 4 == 0 = -1
-      | cc `mod` 4 == 1 = 1
-      | cc `mod` 4 == 2 = 1
-      | otherwise       = -1
-      where
-        s = i
-        a = ((i `div` 4) `mod` 2) /= 0
-        b = ((i `div` 2) `mod` 2) /= 0
-        c = ((i `div` 1) `mod` 2) /= 0
-        xs = [a == False, b == False, a == True, c == False]
-        cc = length [ x | x <- xs, x == False ]
+invsq2 :: Cplx (RootTwo (Ratio Integer))
+invsq2 = Cplx (RootTwo 0 (1 % 2)) 0   :: Cplx (RootTwo (Ratio Integer))
+
+mymatrix :: Matrix Four Four (Cplx (RootTwo (Ratio Integer)))
+mymatrix = matrix  [[ invsq2, 0, invsq2, 0],
+                    [ 0, invsq2, 0, invsq2],
+                    [ 0, invsq2, 0, -invsq2],
+                    [ invsq2, 0, -invsq2, 0]]
 
 synthesized = exact_synthesis mymatrix
 
-r_circuit3 :: Qubit -> Qubit -> Qubit -> Circ ()
-r_circuit3 a b c = do
-  synthesized [a,b,c]
-  return ()
+mybell2 :: Bool -> Bool -> Circ (Qubit, Qubit)
+mybell2 a b = do
+  x <- qinit a 
+  y <- qinit b 
+  synthesized [x,y]
+  return (x,y)
+
+main2 :: IO ()
+main2 = do
+  print_simple Preview (mybell2 False False)
+  
+----------------------------------------------------------------
+-- * Simulate function
+simulate :: Circ (Qubit,Qubit) -> IO ()
+simulate circuit = print (sim_generic (1.0::Float) circuit)
 
 main3 :: IO ()
 main3 = do
-  print_simple Preview r_circuit3
-
-----------------------------------------------------------------
-
-synthesized_conditional :: Matrix Eight Eight Integer  
-synthesized_conditional = matrix_of_function f
-   where
-    f i j
-      | i /= j = 0
-      | i == 0 = 1
-      | otherwise       = -1
-        
-
-synthesized_1 = exact_synthesis synthesized_conditional
-	   
-r_circuit4 :: Qubit -> Qubit -> Qubit -> Qubit -> Qubit -> Qubit -> Circ ()
-r_circuit4 a b c d e f = do
-  synthesized_1 [a,b,c, d, e, f]
-  return () 
+  simulate (mybell1 False)
 
 main4 :: IO ()
 main4 = do
-  print_simple Preview r_circuit4  
-  print synthesized_conditional 
+  simulate (mybell2 False False)
 
-------------------------------------------
-
---conditioned_thing :: Matrix n n Integer
---conditioned_thing = matrix 10 10 
-  
-  
 -- ----------------------------------------------------------------------
 -- * Main
-main = main4
+
+main = do 
+  putStrLn "\n \n choose an option:"
+  putStrLn " 1 - PDF manual implementation of circuit\n"
+  putStrLn " 2 - PDF matrix implementation of circuit\n"
+  putStrLn " 3 - simulation of first circuit\n"
+  putStrLn " 4 - simulation of the second circuit\n"
+  line <- getLine
+  case line of
+    "1" -> do main1
+              main
+    "2" -> do main2
+              main
+    "3" -> do main3
+              main
+    "4" -> do main4
+              main
+    _ -> do main
