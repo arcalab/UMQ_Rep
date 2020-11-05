8154b075b899cd430c2440941e26575ac4c92171
Guillermina Cledou <guillecledou@gmail.com>
2020-11-05 16:29:26 +0000

    Updates

---
+++ precache-manifest.53144f97476c92edef8ce03eb75dd625.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd9c68dda08a484b1fb12eca6930d63",
+    "url": "/quantumsoftwareengineering/index.html"
+  },
+  {
+    "revision": "24d417d401a27e87487c",
+    "url": "/quantumsoftwareengineering/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "5c409b7e44be58c74708",
+    "url": "/quantumsoftwareengineering/static/css/main.717fbd0c.chunk.css"
+  },
+  {
+    "revision": "24d417d401a27e87487c",
+    "url": "/quantumsoftwareengineering/static/js/2.e6c00472.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/quantumsoftwareengineering/static/js/2.e6c00472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c409b7e44be58c74708",
+    "url": "/quantumsoftwareengineering/static/js/main.64efd9b0.chunk.js"
+  },
+  {
+    "revision": "a304ee7cfe718db1ce62",
+    "url": "/quantumsoftwareengineering/static/js/runtime-main.df1b22c2.js"
+  },
+  {
+    "revision": "27fff6d7619626fb83ef7991dcd400f5",
+    "url": "/quantumsoftwareengineering/static/media/arca-logo4.27fff6d7.svg"
+  },
+  {
+    "revision": "08912955de36bfd9ed75eeb632c644e1",
+    "url": "/quantumsoftwareengineering/static/media/bg.08912955.png"
+  },
+  {
+    "revision": "e43299423c396edd2c5fa8db7be65037",
+    "url": "/quantumsoftwareengineering/static/media/haskell.e4329942.png"
+  }
+]);
